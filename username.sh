62bea6404d8d616b29521fd74d511f038c458197
lawrence <lawrence.leymarie@gmail.com>
2020-11-11 13:29:44 -0500

    adds username.sh

---
+++ username.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# username.sh
+# Lawrence
+
+
+echo "Enter a username code: "
+read ZIP
+while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid ZIP code - five digits only!"
+	echo "Enter a five-digit ZIP code: "
+	read ZIP
+done
+echo "Thank you"
