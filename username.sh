e3ae1f15a4ce880e3c11c612f3469b36409107ad
lawrence <lawrence.leymarie@gmail.com>
2020-11-18 01:42:04 -0500

    length 2 no longer accepted

--- username.sh
+++ username.sh
@@ -5,7 +5,7 @@
 
 echo "Enter a username: "
 read -r USERN
-while echo "$USERN" | grep -E -v "^[a-z][a-z0-9_]{1,11}$" > /dev/null 2>&1
+while echo "$USERN" | grep -E -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username, must begin with a lowercase letter, cannot contain upper case must contain digits and underscore answll as be between 3 and 12 characters!"
 	echo "Enter a username: "
