a3bda2af5dfdbe4bbc8cd84dfffa4ce7fba3278b
lawrence <lawrence.leymarie@gmail.com>
2020-11-17 20:39:13 -0500

    username.sh

--- username.sh
+++ username.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 # username.sh
-# Lawrence
+# Lawrenc Leymarie
 
 
-echo "Enter a username code: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+echo "Enter a username: "
+read -r USERN
+while echo "$USERN" | grep -E -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username, must begin with a lowercase letter, cannot contain upper case must contain digits and underscore answll as be between 3 and 12 characters!"
+	echo "Enter a username: "
+	read -r USERN
 done
 echo "Thank you"
